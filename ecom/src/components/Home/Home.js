7b90c3e599bb05fad40baf4003d8351825a5c5c1
ashar-riaz <ashar@123>
2023-12-04 14:07:37 +0500

    Home page add

--- ecom/src/components/Home/Home.js
+++ ecom/src/components/Home/Home.js
@@ -6,9 +6,16 @@ import pic2 from './../../picture/pic2.avif';
 import pic3 from './../../picture/Fashion.png';
 import pic4 from './../../picture/pic4.png';
 import pic5 from './../../picture/pic5.png';
+import delivery from './../../icons/delivery.png';
+import voucher from './../../icons/voucher.png';
+import support from './../../icons/support.png';
+import fashion2 from './../../picture/fashion2.png';
+import fashion1 from './../../picture/fashion1.png';
+import mens from './../../picture/mens.png';
+import women from './../../picture/Women.png';
 
 
-const images = [pic1, pic2,pic3,pic4,pic5];
+const images = [pic1, pic2, pic3, pic4, pic5];
 
 
 export default function Home() {
@@ -43,9 +50,8 @@ export default function Home() {
         <button className="arrow left" onClick={handleLeftArrowClick}>&#8249;</button>
         {images.map((image, index) => (
           <div className="">
-            <img
-              key={index}
-              src={image} // Use image.default to get the correct path
+            <img key={index}
+              src={image}
               alt={`Slide ${index + 1}`}
               className={`slider-image ${index === currentImageIndex ? 'active' : ''}`}
             />
@@ -67,6 +73,42 @@ export default function Home() {
           ))}
         </div>
         <button className="arrow right" onClick={handleRightArrowClick}>&#8250;</button>
+      </div>
+      <div className="d-flex flex-row container">
+        <div className="d-flex boxx">
+          <h4> <span className='mr-4'> <img src={delivery} alt="Delivery" style={{ marginRight: '15px' }} /> </span> Free Delivery</h4>
+        </div>
+        <div className="d-flex boxx">
+          <h4> <img src={voucher} alt="Voucher" style={{ marginRight: '15px' }} /> Use promo code</h4>
+        </div>
+        <div className="d-flex boxx">
+          <h4> <img src={support} alt="Support" style={{ marginRight: '15px' }} /> 24/7 Support</h4></div>
+      </div>
+
+      <div className="d-flex justify-content-center align-items-center mb-3 container">
+        TOP PRODUCT
+      </div>
+      <div className="container mb-3">
+        <div className=" " style={{ width: '100%', height: '250px', backgroundColor: 'red' }}></div>
+      </div>
+      <div className="d-flex main-box container">
+        <div className="d-flex">
+          <img src={mens} alt="" style={{ width: '350px' }} />
+        </div>
+        <div className="d-flex flex-column">
+          <div className="d-flex mb-3">
+            <img src={fashion1} alt="" style={{ width: '550px', height: '240px' }} />
+
+          </div>
+          <div className="d-flex">
+            <img src={fashion2} alt="" style={{ width: '550px', height: '240px' }} />
+
+          </div>
+        </div>
+        <div className="d-flex">
+          <img src={women} alt="" style={{ width: '350px' }} />
+        </div>
+
       </div>
     </>
   );
